3215123209c9790f398f1e7c2dcbfcf58bbabb3d
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-09 10:31:52 +0300

    First react commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Greetings, { Greetings2 } from "./components/greetings";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Greetings />
+      <Greetings2></Greetings2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/greetings.jsx
@@ -0,0 +1,7 @@
+export default function Greetings() {
+  return <h2>Hello</h2>;
+}
+
+export function Greetings2() {
+  return <h3>The second greeting from basic export</h3>;
+}
